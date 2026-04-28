b069c5d666446c92d4e90c9d9aaf511406fd1fc0
363434879 <363434879@gotvdsb.ca>
2026-04-28 13:13:56 -0400

    Add project files.

---
+++ Monogame Lesson 3 – Animation Speed and Vectors/Game1.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Lesson_3___Animation_Speed_and_Vectors
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        Rectangle greyTribbleRect;
+
+        Texture2D tribbleGreyTexture;
+
+        Vector2 tribbleGreySpeed;
+
+        SoundEffect tribbleCoo;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "Animation Speed & Vectors";
+
+            greyTribbleRect = new Rectangle (300, 10, 100, 100);
+            tribbleGreySpeed = new Vector2(2, 2);
+           
+            base.Initialize();
+
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            tribbleGreyTexture = Content.Load<Texture2D>("tribbleGrey");
+
+            tribbleCoo = Content.Load<SoundEffect>("tribble_coo");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            greyTribbleRect.X += (int)tribbleGreySpeed.X;
+            greyTribbleRect.Y += (int)tribbleGreySpeed.Y;
+
+            if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
+            {
+                tribbleGreySpeed.X *= -1;
+                tribbleCoo.Play();
+
+            }
+
+
+            if (greyTribbleRect.Bottom > window.Height || greyTribbleRect.Top < 0)
+            {
+                tribbleGreySpeed.Y *= -1;
+                tribbleCoo.Play();
+            }
+          
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(tribbleGreyTexture, greyTribbleRect, Color.White);
+            _spriteBatch.End();
+           
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Monogame Lesson 3 – Animation Speed and Vectors/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Monogame_Lesson_3___Animation_Speed_and_Vectors.Game1();
+game.Run();
